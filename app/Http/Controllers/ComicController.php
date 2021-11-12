0c8bdc8a6a2d2818f61e818d63103170459f67ea
brunEdo <edobruno.m@gmail.com>
2021-11-12 17:47:58 +0100

    Crud methods control

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -60,10 +60,8 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Comic $comic)
     {
-        $comic = Comic::find($id);
-
         return view("comics.show", compact("comic"));
     }
 
@@ -73,9 +71,9 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Comic $comic)
     {
-        //
+        return view("comic.edit", compact("comic"));
     }
 
     /**
@@ -85,9 +83,13 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comic $comic)
     {
-        //
+        $data = $request->all();
+
+        $comic->update($data);
+
+        return redirect()->route("comics.show", $comic->id);
     }
 
     /**
@@ -96,8 +98,10 @@ class ComicController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Comic $comic)
     {
-        //
+        $comic->delete();
+
+        return redirect()->route("comics.index");
     }
 }
